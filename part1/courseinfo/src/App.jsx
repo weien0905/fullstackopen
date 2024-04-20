a7e54b19bb01ec52f321eb6f40b635b953695fdc
weien0905 <weien0905@hotmail.my>
2024-04-20 13:42:08 +0800

    Initial commit

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,55 @@
+const Course = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part part={props.parts[0].part} exercise={props.parts[0].exercise} />
+      <Part part={props.parts[1].part} exercise={props.parts[1].exercise} />
+      <Part part={props.parts[2].part} exercise={props.parts[2].exercise} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.exercises.reduce((a, c) => a + c, 0)}</p>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+        {props.part} {props.exercise}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Course course={course} />
+      <Content parts={[
+        {part: part1, exercise: exercises1},
+        {part: part2, exercise: exercises2},
+        {part: part3, exercise: exercises3}
+      ]}/>
+      <Total exercises={
+        [exercises1, exercises2, exercises3]
+      } />
+    </div>
+  )
+}
+
+export default App
