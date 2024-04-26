aa370ef51389e405e5675afd50116c6930e1a9d4
weien0905 <weien0905@hotmail.my>
2024-04-27 00:21:34 +0800

    Submission for exercise 2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]); 
+  const [newName, setNewName] = useState('');
+
+  const addName = e => {
+    e.preventDefault();
+    setPersons([...persons, {name: newName}]);
+    setNewName('');
+  }
+
+  const handleNameChange = e => {
+    setNewName(e.target.value);
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => 
+        <div key={person.name}>{person.name}</div>
+      )}
+    </div>
+  )
+}
+
+export default App
