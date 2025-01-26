ed5649444aa43e2a22046ea30653cc7385287875
weien0905 <weien0905@hotmail.my>
2025-01-26 19:33:44 +0800

    Submission for exercise 3.4 - 3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,56 +1,102 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+app.use(express.json());
 
 let persons = [
     { 
-      "id": 1,
+      "id": "1",
       "name": "Arto Hellas", 
       "number": "040-123456"
     },
     { 
-      "id": 2,
+      "id": "2",
       "name": "Ada Lovelace", 
       "number": "39-44-5323523"
     },
     { 
-      "id": 3,
+      "id": "3",
       "name": "Dan Abramov", 
       "number": "12-43-234345"
     },
     { 
-      "id": 4,
+      "id": "4",
       "name": "Mary Poppendieck", 
       "number": "39-23-6423122"
     }
-]
+];
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
-})
+    response.json(persons);
+});
 
-app.get('/api/info', (request, response) => {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date(Date.now()).toString()}</p>`)
-})
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${Date(Date.now()).toString()}</p>`);
+});
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
+  const id = request.params.id;
+  const person = persons.find(person => person.id === id);
   
   if (person) {
-    response.json(person)
+    response.json(person);
   } else {
-    response.status(404).end()
+    response.status(404).end();
   }
-})
+});
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+  const id = request.params.id;
+  persons = persons.filter(person => person.id !== id);
 
-  response.status(204).end()
-})
+  response.status(204).end();
+});
 
-const PORT = 3001
+const generateId = () => {
+  while (true) {
+    // Generate random number between 1 to 10,000 
+    let id = String(Math.floor(Math.random() * 10000) + 1); 
+
+    // Check if there is duplicate in ID
+    if (!persons.some(person => person.id === id)) {
+      return id;
+    }
+  }
+};
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'Name is missing' 
+    });
+  }
+
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'Number is missing' 
+    });
+  }
+
+  if (persons.some(person => person.name === body.name)) {
+    return response.status(400).json({ 
+      error: 'Name must be unique' 
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+}); 
+
+const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
