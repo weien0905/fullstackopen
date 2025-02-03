04914655b383707f370d6bc2b5bb409017788771
weien0905 <weien0905@hotmail.my>
2025-02-03 20:57:53 +0800

    Submission for exercise 3.9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
+const cors = require('cors');
+
+app.use(cors());
+
 app.use(express.json());
 
 morgan.token('body', req => {
@@ -103,7 +107,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person);
 }); 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
